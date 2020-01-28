d4fc0cd982afb92ce30236ce7fe9b1ce7a38d7bf
Cristiane11 <bjwmcd@yahoo.com>
2020-01-28 04:44:38 -0500

    update

--- robot2/src/index.js
+++ robot2/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import CardList from './CardList';
+import CardList2 from './CardList2';
 import * as serviceWorker from './serviceWorker';
 import { robots } from './robots';
 
-ReactDOM.render(<CardList robots={robots}/>,
+ReactDOM.render(<CardList2 robots={robots}/>,
         
  document.getElementById('root'));
 
