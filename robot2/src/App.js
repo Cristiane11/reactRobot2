8a8b4a1a34168e9163f65501bcf694e785a06c07
Cristiane11 <bjwmcd@yahoo.com>
2020-01-28 05:10:25 -0500

    update

--- robot2/src/App.js
+++ robot2/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { robots } from './robots';
 import CardList2 from './CardList2';
+import SearchBox from './SearchBox';
 const App =()=>{
     return(
         <div>
             <h1>Robot Friends</h1>
+            <SearchBox/>
             <CardList2 robots={robots}/>
         </div>
         );
