4c04ba5eceb84ce4ae352a9e5487967c706814c2
Cristiane11 <bjwmcd@yahoo.com>
2020-01-29 05:36:58 -0500

    upadate

--- robot2/src/App.js
+++ robot2/src/App.js
@@ -4,7 +4,7 @@ import CardList2 from './CardList2';
 import SearchBox from './SearchBox';
 const App =()=>{
     return(
-        <div>
+        <div className='tc'>
             <h1>Robot Friends</h1>
             <SearchBox/>
             <CardList2 robots={robots}/>
