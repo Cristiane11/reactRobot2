15e681cc46202378d3ef7909160f060d7f22db04
Cristiane11 <bjwmcd@yahoo.com>
2020-01-30 05:22:02 -0500

    update

--- robot2/src/App2.js
+++ robot2/src/App2.js
@@ -3,15 +3,20 @@ import React,{Component} from 'react';
 import { robots } from './robots';
 import CardList2 from './CardList2';
 import SearchBox from './SearchBox';
+
 const state = {
     robots: robots,
     searchfield:''
 }
-export App2 extends Component{
-    return(
-        
-    )
+class App2 extends Component{
+    render(){  
+        return(
+        <div className='tc'>
+        <h1>Robot Friends</h1>
+        <SearchBox/>
+        <CardList2 robots={robots}/>
+    </div>
+    );
+  }   
 }
-
-
 export default App2;
