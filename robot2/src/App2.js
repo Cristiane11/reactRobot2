12237c4ed8faa734503d969fcfd0bedbd7a62a12
Cristiane11 <bjwmcd@yahoo.com>
2020-01-30 05:11:43 -0500

    update

---
+++ robot2/src/App2.js
@@ -0,0 +1,17 @@
+// here is a App component with STATE
+import React,{Component} from 'react';
+import { robots } from './robots';
+import CardList2 from './CardList2';
+import SearchBox from './SearchBox';
+const state = {
+    robots: robots,
+    searchfield:''
+}
+export App2 extends Component{
+    return(
+        
+    )
+}
+
+
+export default App2;
